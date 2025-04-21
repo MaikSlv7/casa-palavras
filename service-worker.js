fec4418da2e71a0c2c3a20f631c5e37fa65f6daf
Maik Silva <silvamaik52@gmail.com>
2025-04-21 12:51:57 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,16 +3,16 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('caca-palavras-v1').then(cache => {
       return cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './script.js',
-        './manifest.json',
-        './icon-512.png',
-        './click.wav',
-        './ding.wav',
-        './erro.wav',
-        './tempo.wav'
+        '/casa-palavras/',
+        '/casa-palavras/index.html',
+        '/casa-palavras/style.css',
+        '/casa-palavras/script.js',
+        '/casa-palavras/manifest.json',
+        '/casa-palavras/icon-512.png',
+        '/casa-palavras/click.wav',
+        '/casa-palavras/ding.wav',
+        '/casa-palavras/erro.wav',
+        '/casa-palavras/tempo.wav'
       ]);
     })
   );
